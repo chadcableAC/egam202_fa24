ae2c918ca75719fb16bdc45a011e8614180b3193
Chad (macOS) <chadcable@gmail.com>
2024-09-24 11:56:59 -0400

    --- Working randomizer

--- egam202_fa24/Assets/Randomizer.cs
+++ egam202_fa24/Assets/Randomizer.cs
@@ -7,26 +7,62 @@ public class Randomzier : MonoBehaviour
     [System.Serializable]
     public class Data
     {
-        public string name;
+        public string student;
+        public bool isPresent;
     }
 
     public Data[] datas;
+    public bool isPlaytesting;
 
-
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Randomize();        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Randomize()
     {
-        
+        // Build the list of present students
+        List<Data> finalDatas = new();
+        foreach (Data data in datas)
+        {
+            if (data.isPresent)
+            {
+                finalDatas.Add(data);
+            }
+        }
+
+        // Randomize
+        Shuffle(finalDatas);
+
+        // Spell out the order        
+        string output = string.Empty;
+
+        for (int i = 0; i < finalDatas.Count; i++)
+        {
+            output += $"{i + 1}: {finalDatas[i].student}\n";
+
+            if (isPlaytesting)
+            {
+                int adjIndex = (i + 1) % finalDatas.Count;
+                output += $"-> playtested by: {finalDatas[adjIndex].student}\n";
+            }
+
+            output += "\n";
+        }
+
+        Debug.Log(output);
+    }
+
+    public void Shuffle<T>(IList<T> ts)
+    {
+        var count = ts.Count;
+        var last = count - 1;
+        for (var i = 0; i < last; ++i)
+        {
+            var r = UnityEngine.Random.Range(i, count);
+            var tmp = ts[i];
+            ts[i] = ts[r];
+            ts[r] = tmp;
+        }
     }
 }
-
-
-
